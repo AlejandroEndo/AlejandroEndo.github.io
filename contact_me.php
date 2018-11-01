61e7fc346521a98c8448ee53d2de1e6a252d76a0
Alejandro Endo <alejoendo@hotmail.com>
2018-11-01 12:42:30 -0500

    .

--- contact_me.php
+++ contact_me.php
@@ -1,21 +1,14 @@
-<?php
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
+<?php 
+$myemail = 'misakinya2@gmail.com';
 $name = $_POST['name'];
-$email_address = $_POST['email'];
+$email = $_POST['email'];
 $message = $_POST['message'];
-	
-$to = 'alejoendo@hotmail.com';
-$email_subject = "$name, te ha contactado";
-$email_body = "Se ha recibido un mensaje a traves del formulario de Contacto del sitio Web.\n\n"."Datos:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+
+$to = $myemail;
+$email_subject = "Nuevo mensaje de: $subject";
+$email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mensaje: \n $message";
+$headers = "From: $email";
+
+mail($to, $email_subject, $email_body, $headers);
+echo "El mensaje se ha enviado correctamente";
 ?>
